abba993658e0be6ef37be0d962415d458adcb154
siwookim98 <44838722+siwookim98@users.noreply.github.com>
2026-08-10 00:57:23 +0900

    [Passed] 비교 연산 (48ms, 16MB)

--- trail0/비교 연산/comparison-operator.py
+++ trail0/비교 연산/comparison-operator.py
@@ -1,31 +1,38 @@
 A, B = map(int, input().split())
 
-if A >= B:
-    print(1)
-else:
-    print(0)
+# if A >= B:
+#     print(1)
+# else:
+#     print(0)
 
-if A > B:
-    print(1)
-else:
-    print(0)
+# if A > B:
+#     print(1)
+# else:
+#     print(0)
 
-if A <= B:
-    print(1)
-else:
-    print(0)
+# if A <= B:
+#     print(1)
+# else:
+#     print(0)
 
-if A < B:
-    print(1)
-else:
-    print(0)
+# if A < B:
+#     print(1)
+# else:
+#     print(0)
 
-if A == B:
-    print(1)
-else:
-    print(0)
+# if A == B:
+#     print(1)
+# else:
+#     print(0)
 
-if A != B:
-    print(1)
-else:
-    print(0)
+# if A != B:
+#     print(1)
+# else:
+#     print(0)
+
+print(int(A >= B))
+print(int(A > B))
+print(int(B >= A))
+print(int(B > A))
+print(int(A == B))
+print(int(A != B))
