54873200627b6dca5f68b745af6c86e357d14dd8
siwookim98 <44838722+siwookim98@users.noreply.github.com>
2026-09-20 01:43:04 +0900

    [Passed] 알파벳에 따른 평가 (46ms, 16MB)

---
+++ trail1/알파벳에 따른 평가/evaluation-by-alphabet.py
@@ -0,0 +1,13 @@
+A = str(input())
+if A == 'S':
+    print('Superior')
+elif A == 'A':
+    print('Excellent')
+elif A == 'B':
+    print('Good')
+elif A == 'C':
+    print('Usually')
+elif A == 'D':
+    print('Effort')
+else:
+    print('Failure')
